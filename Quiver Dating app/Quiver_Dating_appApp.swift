3c5832d1b0ec6426be767c677ca8fe936a9fe237
Alex <sangawere1@gmail.com>
2024-05-11 17:16:17 +0300

    Initial Commit

---
+++ Quiver Dating app/Quiver_Dating_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiver_Dating_appApp.swift
+//  Quiver Dating app
+//
+//  Created by стасик on 11.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Quiver_Dating_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
